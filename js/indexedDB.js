9fd37b7bec5ab28a556be696767d8b04ce96ba62
LuixSlv <155217216+LuixSlv@users.noreply.github.com>
2025-02-14 12:02:23 -0300

    Update indexedDB.js

--- js/indexedDB.js
+++ js/indexedDB.js
@@ -1,32 +1,49 @@
+const dbName = "PokemonGameDB";
 let db;
 
-function openDatabase() {
-    let request = indexedDB.open("pokemonGameDB", 1);
+function openDB() {
+    let request = indexedDB.open(dbName, 1);
 
     request.onupgradeneeded = function(event) {
-        let db = event.target.result;
-        db.createObjectStore("saves", { keyPath: "id" });
+        db = event.target.result;
+        if (!db.objectStoreNames.contains("saves")) {
+            db.createObjectStore("saves", { keyPath: "id" });
+        }
     };
 
     request.onsuccess = function(event) {
         db = event.target.result;
+    };
+
+    request.onerror = function(event) {
+        console.log("Erro ao abrir IndexedDB:", event.target.errorCode);
     };
 }
 
 function saveGame(data) {
     let transaction = db.transaction(["saves"], "readwrite");
     let store = transaction.objectStore("saves");
-    store.put(data);
+    store.put({ id: "player", ...data });
 }
 
 function loadGame(callback) {
     let transaction = db.transaction(["saves"], "readonly");
     let store = transaction.objectStore("saves");
-    let request = store.get(1);
+    let request = store.get("player");
 
     request.onsuccess = function() {
-        callback(request.result);
+        callback(request.result || null);
+    };
+
+    request.onerror = function() {
+        callback(null);
     };
 }
 
-openDatabase();
+function clearIndexedDB() {
+    let transaction = db.transaction(["saves"], "readwrite");
+    let store = transaction.objectStore("saves");
+    store.clear();
+}
+
+openDB();
